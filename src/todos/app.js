d0ef52793c1ad5776a100eba61702d743120bd94
Miguel Mateo <miguelmateot94@gmail.com>
2023-02-02 10:10:39 -0400

    List new todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -3,7 +3,8 @@ import html from "./app.html?raw";
 import { renderTodos } from "./use-cases";
 
 const ElementIds = {
-    TodoList: '.todo-list'
+    TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input'
 }
 
 /**
@@ -23,4 +24,18 @@ export const App = (elementId) => {
         document.querySelector(elementId).append( app );
         renderTodo();
     })();
+
+    // Referencias HTML
+
+    const newDescriptionInput = document.querySelector( ElementIds.NewTodoInput );
+
+    //Listeners
+    newDescriptionInput.addEventListener('keyup', (event) => {
+        if( event.keyCode !== 13 ) return;
+        if( event.target.value.trim().length === 0 ) return;
+
+        todoStore.addTodo( event.target.value );
+        renderTodo();
+        event.target.value = '';
+    })
 }
--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,5 +1,7 @@
 import { createTodoHtml } from "./create-todo-html";
 
+let element;
+
 /**
  * 
  * @param {String} elementId 
@@ -7,8 +9,13 @@ import { createTodoHtml } from "./create-todo-html";
  */
 export const renderTodos = ( elementId, todos = [] ) => {
 
-    //TODO: referencia
-    const element = document.querySelector( elementId );
+    if( !element )
+        element = document.querySelector( elementId );
+
+    if ( !element )
+        throw new Error(`Element ${ elementId } not found`);
+
+    element.innerHTML = '';
 
     todos.forEach(todo => {
         element.append( createTodoHtml(todo) );
