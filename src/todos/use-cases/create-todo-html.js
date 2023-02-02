426849e45a45f434964f2d91c89766cdd77b7075
Miguel Mateo <miguelmateot94@gmail.com>
2023-02-01 23:46:17 -0400

    Rendering html

---
+++ src/todos/use-cases/create-todo-html.js
@@ -0,0 +1,15 @@
+
+/**
+ * 
+ * @param {Todo} todo 
+ */
+export const createTodoHtml = ( todo ) => {
+    if( !todo ) throw new Error('A TODO object is required');
+
+    const html = `<h1>${todo.description}</h1>`;
+
+    const liElement = document.createElement('li');
+    liElement.innerHTML = html
+
+    return liElement;
+}
---
+++ src/todos/use-cases/render-todos.js
@@ -0,0 +1,17 @@
+import { createTodoHtml } from "./create-todo-html";
+
+/**
+ * 
+ * @param {String} elementId 
+ * @param {Todo} todos 
+ */
+export const renderTodos = ( elementId, todos = [] ) => {
+
+    //TODO: referencia
+    const element = document.querySelector( elementId );
+
+    todos.forEach(todo => {
+        element.append( createTodoHtml(todo) );
+    });
+
+}
